4f86c17fbbca80a7ba7912689d033adcda6ac002
程歌 <1986267634@qq.com>
2018-06-20 22:44:38 +0800

    note

---
+++ express_apache/app.js
@@ -0,0 +1,61 @@
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
+
+//入口文件
+//模拟静态资源服务器（Apache服务器）
+
+//加载express模块
+
+var express = require('express');
+var path = require('path');
+
+//创建app对象
+var app = express();
+
+//注册路由
+//express.static处理静态资源的函数
+app.use('/',express.static('./public'));
+
+//启动服务
+app.listen(9999,function(){
+    console("http:localhost:9999");
+});
